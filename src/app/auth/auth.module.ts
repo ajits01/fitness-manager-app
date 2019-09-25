e493966846046e34412fc57e51a9b588f19a7635
Ajit <ajits01@outlook.com>
2019-09-26 01:37:29 +0530

    04-Angularfire-Integration

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginModule } from './login/login.module';
-import { RegisterModule } from './register/register.module';
+
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+// shared modules
 import { SharedModule } from './shared/shared.module';
 
 export const ROUTES: Routes = [
@@ -24,13 +29,26 @@ export const ROUTES: Routes = [
   }
 ];
 
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: '',
+  authDomain: 'fitness-manager-app-8d32f.firebaseapp.com',
+  databaseURL: 'https://fitness-manager-app-8d32f.firebaseio.com',
+  projectId: 'fitness-manager-app-8d32f',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+};
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(ROUTES),
-    LoginModule,
-    RegisterModule,
-    SharedModule
-  ]
+    RouterModule.forChild(ROUTES),
+    SharedModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  exports: [RouterModule]
 })
 export class AuthModule {}
