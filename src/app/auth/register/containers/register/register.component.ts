e7a27daca6936dd50f98c06edfffcb56ec1cf03a
Ajit <ajits01@outlook.com>
2019-09-25 23:18:39 +0530

    03-Auth-Components

---
+++ src/app/auth/register/containers/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'fma-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  registerUser(event: FormGroup) {
+    console.log(event.value);
+  }
+}
